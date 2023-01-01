90af5db6da3ac625b3bfabac4aefc7b46df98af8
andoski99 <goodsportdevelopment@icloud.com>
2023-01-01 17:22:10 +1100

    JSON working from Filemaker

--- src/index.js
+++ src/index.js
@@ -2,34 +2,42 @@
 
 import c3 from "c3";
 
-window.loadChart = function () {
+window.loadChart = function (json) {
 
-data = [
-  {
-    month: "Jan",
-    Apples: 327,
-    Peaches: 437,
-    Pears: 111,
-  },
-  {
-    month: "Feb",
-    Apples: 555,
-    Peaches: 454,
-    Pears: 222,
-  },
-  {
-    month: "Mar",
-    Apples: 242,
-    Peaches: 864,
-    Pears: 343,
-  },
-  {
-    month: "Apr",
-    Apples: 222,
-    Peaches: 454,
-    Pears: 434,
-  },
-];
+console.log ("loadChart",json);
+
+const obj = JSON.parse(json);
+
+const data = obj.data;
+
+console.log(obj);
+
+// data = [
+//   {
+//     month: "Jan",
+//     Apples: 327,
+//     Peaches: 437,
+//     Pears: 111,
+//   },
+//   {
+//     month: "Feb",
+//     Apples: 555,
+//     Peaches: 454,
+//     Pears: 222,
+//   },
+//   {
+//     month: "Mar",
+//     Apples: 242,
+//     Peaches: 864,
+//     Pears: 343,
+//   },
+//   {
+//     month: "Apr",
+//     Apples: 222,
+//     Peaches: 454,
+//     Pears: 434,
+//   },
+// ];
 
 const options = {
   bindto: "#chart",
